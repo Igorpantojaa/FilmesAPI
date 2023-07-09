bf03e286c5ea59bd9bf65c8744edb835c5317704
Igorpantojaa <96669797+Igorpantojaa@users.noreply.github.com>
2023-07-09 17:54:41 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Data/DTOs/UpdateFilmeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.DTOs;
+
+public class UpdateFilmeDTO
+{
+    [Required(ErrorMessage = "O títiulo do filme é obrigatório.")]
+    [StringLength(50, ErrorMessage = "O título do filme não pode exceder o limite de 50 caracteres.")]
+    public string? Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O gênero do filme não pode exceder o limite de 50 caracteres.")]
+    public string? Genero { get; set; }
+    [Required(ErrorMessage = "A duração do filme é obrigatória.")]
+    [Range(1, 360, ErrorMessage = "A duração do filme deve ser entre 1 e 360 minutos.")]
+    public int Duracao { get; set; }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O títiulo do filme é obrigatório.")]
+    [MaxLength(50, ErrorMessage ="O título do filme não pode exceder o limite de 50 caracteres.")]
+    public string? Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O gênero do filme não pode exceder o limite de 50 caracteres.")]
+    public string? Genero { get; set; }
+    [Required(ErrorMessage = "A duração do filme é obrigatória.")]
+    [Range(1, 360, ErrorMessage ="A duração do filme deve ser entre 1 e 360 minutos.")]
+    public int Duracao { get; set; }
+}
